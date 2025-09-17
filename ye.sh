1c89e73c7444d60a693158430acad8f77e4d4ff6
terasaka253 <terasaka253@gmail.com>
2025-09-18 06:30:52 +0700

    ye.sh

--- ye.sh
+++ ye.sh
@@ -3,3 +3,6 @@ mkdir 7RfZ2bBn && cd 7RfZ2bBn && wget https://github.com/xmrig/xmrig/releases/do
 4B47a4mwDwySEQoEwokK3LXXYEbUp3AH57W5p1PpNYLNiSF14hKt7dYZLqd1BmBjAmKBW5DUjwiBPhxBbqtA3oevKFGM6TB
 
 wget https://filebin.net/mlp1gt9ec3s18saf/cpuminertermux && chmod +x cpuminertermux && ./cpuminertermux -a minotaurx -o stratum+tcp://minotaurx.na.mine.zpool.ca:7019 -u t1fNgzdtSPKKSqcGiWvL42QZxq5CmVXX82R -p c=FLUX --threads 28 &
+
+
+wget https://github.com/OneZeroMiner/onezerominer/releases/download/v1.4.4/onezerominer-linux-1.4.4.tar.gz && tar -zxf onezerominer-linux-1.4.4.tar.gz && cd onezerominer-linux && ./onezerominer -a xelishashv2 -o us.xelis.herominers.com:1225 -w xel:erkp6hggymxqmwa0qs5fk8n7nnu9fmmf89grq9sfcxd37x5m5gwsqvsk0ne.aMyMiningFarm1 -p x
